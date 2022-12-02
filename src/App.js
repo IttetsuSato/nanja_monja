e5752d5cf7f57e108ddb7a7563a8c1a0c5070a09
Ittetsu Sato <ittetsu.sato@uzabase.com>
2022-12-02 20:23:50 +0900

    feat: chakraを追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <p>他の人より早く出てきたカードの名前を呼ぼう！</p>
+        <p>初めてみるカードには名前をつけよう！</p>
+      </div>
+    </ChakraProvider>
   );
 }
 
