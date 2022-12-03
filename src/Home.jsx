c7c75b55d813a13a376d1a159a870de7b49f07ec
Ittetsu Sato <ittetsu.sato@uzabase.com>
2022-12-03 15:35:48 +0900

    feat: 背景画像を整える

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,6 +7,9 @@ const IMAGES = 12;
 const Card = styled.div`
   width: 320px;
   height: 320px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   border-radius: 16px;
   overflow: hidden;
@@ -18,10 +21,18 @@ const Card = styled.div`
   left: 0;
 `;
 const CardFront = styled(Card)`
+  width: 320px;
+  height: 320px;
   transform: ${(props) => (props.isOpen ? "rotateY(0)" : "rotateY(-180deg)")};
 `;
 const CardBack = styled(Card)`
+  width: 320px;
+  height: 320px;
   transform: ${(props) => (props.isOpen ? "rotateY(180deg)" : "rotateY(0)")};
+  background-image: linear-gradient(
+    rgba(0, 0, 255, 0.5),
+    rgba(255, 255, 0, 0.5)
+  );
 `;
 const Home = () => {
   const [imageNumber, setImageNumber] = useState(
@@ -47,20 +58,14 @@ const Home = () => {
       <Heading>カードの名前を他の人より早く呼ぼう！</Heading>
       <Heading>初めてみるカードには名前をつけよう！</Heading>
       <Flex justifyContent="center">
-        <Box
-          pos="relative"
-          w="320px"
-          onClick={() => onHandleNext()}
-        >
+        <Box pos="relative" w="320px" onClick={() => onHandleNext()}>
           <CardFront isOpen={isOpen}>{image}</CardFront>
           <CardBack isOpen={isOpen}>
-            <Flex justifyContent="center" alignItems="center">
               <Image
                 w="60%"
                 src={`${process.env.PUBLIC_URL}/img/kids_chuunibyou_girl.png`}
                 alt="読み込みエラーです、、、"
               />
-            </Flex>
           </CardBack>
         </Box>
       </Flex>
